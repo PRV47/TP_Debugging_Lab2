148abba8f126f43fa46a03cd6417b14754e19ad0
Piers <57373307+PRV47@users.noreply.github.com>
2024-05-30 11:59:19 -0300

    Add files via upload
    Ejercicios 5, 6a y 6b

---
+++ TP_Debugging/src/DebugExample.java
@@ -0,0 +1,20 @@
+public class DebugExample {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int result = add(a, b);
+        int result2 = multiply(a, b);
+        System.out.println("Result: " + result);
+        System.out.println("Result: " + result2);
+    }
+
+    public static int add(int x, int y) {
+        int sum = x + y;
+        return sum;
+    }
+
+    public static int multiply(int x, int y) {
+        int product = x * y;
+        return product;
+    }
+}
